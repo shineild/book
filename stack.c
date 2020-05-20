3ab0dcfdb7d793f9eafc585f764b6ea7c3882fa3
seo ho jin <aku7777@naver.com>
2020-05-21 00:53:06 +0900

    stack_information

--- stack.c
+++ stack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define ANSI_COLOR_BLUE "\x1b[34m"
 
 void banner(){
 
@@ -10,6 +11,24 @@ void banner(){
 }
 
 void stack_information(){
+
+    puts("------- stack 이란? ------\n");
+    puts("한 쪽 끝에서만 데이터를 넣고 뺄 수 있는 LIFO(Last In First Out) 형식의 자료 구조로");
+    puts("가장 최근에 삽입한 데이터가 가장 먼저 제거되는 자료구조이다.");
+
+    puts("\n\n");
+
+    puts("                           push 9!");
+    puts("                      --------------");
+    puts("                      |      9     |            pop!");
+    puts(" --------------       --------------       --------------       ");
+    puts(" |      5     |       |      5     |       |      5     |            pop!");
+    puts(" --------------       --------------       --------------       --------------");
+    puts(" |      1     |       |      1     |       |      1     |       |      1     |");
+    puts(" --------------       --------------       --------------       --------------");
+    puts(" |      6     |       |      6     |       |      6     |       |      6     |");
+    puts(" --------------       --------------       --------------       --------------");
+    puts("\n\n");
 
 }
 
@@ -23,7 +42,7 @@ int main(){
         if(choice == 1){
             stack_information();
         }
-        else if(choice == 4){
+        else if(choice == 3){
             break;
         }
 
